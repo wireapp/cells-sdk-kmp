891313aceb0d55a9200f1c3f38a2b6e868644794
Bruno Sinou <bruno.sinou@posteo.de>
2025-02-04 18:31:26 +0100

    More logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,11 +82,17 @@ mavenPublishing {
 
 signing {
     val b64Key = System.getenv("BASE64_PGP_KEY") ?: ""
+    val signing = System.getenv("SIGN_ARTIFACTS") ?: ""
+
+    warning("=== Env variable, signing: $signing")
+
     if (b64Key.isNotEmpty()){
+        warning("=== Env variable, b6pk: ${b64Key.subSequence(0,8)}...")
+
         val pgpKey = String(Base64.getDecoder().decode(b64Key), StandardCharsets.UTF_8)
         useInMemoryPgpKeys(pgpKey, System.getenv("ORG_GRADLE_PROJECT_signingInMemoryKeyPassword"))
     } else {
-        warning("PGP_KEY_BASE64 is not set, cannot sign.")
+        warning("BASE64_PGP_KEY is not set, cannot sign.")
     }
 }
 
