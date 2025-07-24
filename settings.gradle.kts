1eab46667673469a6586316df4f519e868a4bba8
Bruno Sinou <bruno.sinou@posteo.de>
2025-07-24 13:38:09 +0200

    chore(tps): update AWS SDK v2 BOM

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("awssdk") {
-            from("aws.sdk.kotlin:version-catalog:1.4.111")
+            from("aws.sdk.kotlin:version-catalog:1.4.125")
         }
     }
 }
